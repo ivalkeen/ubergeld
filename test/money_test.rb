901e148670fcb422a951cde0d49732029557ef42
Ivan Tkalin <itkalin@gmail.com>
2015-09-30 20:43:12 +0200

    (test) Add more amount/currency tests

--- test/money_test.rb
+++ test/money_test.rb
@@ -4,16 +4,25 @@ class MoneyTest < Minitest::Test
   def test_amount
     money = Money.new(50, "EUR")
     assert_equal(50, money.amount)
+
+    money = Money.new(20, "USD")
+    assert_equal(20, money.amount)
   end
 
   def test_currency
     money = Money.new(50, "EUR")
     assert_equal("EUR", money.currency)
+
+    money = Money.new(20, "USD")
+    assert_equal("USD", money.currency)
   end
 
   def test_inspect
     money = Money.new(50, "EUR")
     assert_equal("50.00 EUR", money.inspect)
+
+    money = Money.new(20.5, "USD")
+    assert_equal("20.50 USD", money.inspect)
   end
 
   def test_conversion_rates_setting
