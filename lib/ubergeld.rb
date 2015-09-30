bc531af8912a6c1e125f007017ebce5761226cf2
Ivan Tkalin <itkalin@gmail.com>
2015-09-30 20:55:11 +0200

    (feat) Add naive conversion rates implementation

--- lib/ubergeld.rb
+++ lib/ubergeld.rb
@@ -5,8 +5,12 @@ require "bigdecimal/util"
 
 class Money
   class << self
-    def conversion_rates(_, _)
+    def conversion_rates(base_currency, rates)
+      @base_currency = base_currency
+      @rates = rates.freeze
     end
+
+    attr_reader :rates, :base_currency
   end
 
   attr_reader :amount, :currency
@@ -18,5 +22,23 @@ class Money
 
   def inspect
     format("%#.2f #{currency}", amount)
+  end
+
+  def convert_to(target_currency)
+    in_base_currency = nil
+
+    if currency == Money.base_currency
+      in_base_currency = amount
+    else
+      to_base_rate = Money.rates[currency]
+      in_base_currency = amount / to_base_rate
+    end
+
+    if target_currency == Money.base_currency
+      Money.new(in_base_currency, Money.base_currency)
+    else
+      to_target_rate = Money.rates[target_currency]
+      Money.new(in_base_currency * to_target_rate, target_currency)
+    end
   end
 end
--- test/money_test.rb
+++ test/money_test.rb
@@ -1,6 +1,18 @@
 require "test_helper"
 
 class MoneyTest < Minitest::Test
+  Money.conversion_rates(
+    "EUR",
+    "USD" => 1.11,
+    "Bitcoin" => 0.0047,
+  )
+
+  def test_conversion_rates_set
+    assert_equal("EUR", Money.base_currency)
+    assert_equal(1.11, Money.rates["USD"])
+    assert_equal(0.0047, Money.rates["Bitcoin"])
+  end
+
   def test_amount
     money = Money.new(50, "EUR")
     assert_equal(50, money.amount)
@@ -25,11 +37,21 @@ class MoneyTest < Minitest::Test
     assert_equal("20.50 USD", money.inspect)
   end
 
-  def test_conversion_rates_setting
-    Money.conversion_rates(
-      "EUR",
-      "USD" => 1.11,
-      "Bitcoin" => 0.0047,
-    )
+  def test_convert_from_base_currency
+    money = Money.new(50, "EUR")
+    money_in_dollars = money.convert_to("USD")
+    assert_equal("55.50 USD", money_in_dollars.inspect)
+  end
+
+  def test_convert_to_base_currency
+    money_in_dollars = Money.new(55.5, "USD")
+    money = money_in_dollars.convert_to("EUR")
+    assert_equal("50.00 EUR", money.inspect)
+  end
+
+  def test_convert_between_non_base_currencies
+    money_in_dollars = Money.new(50, "USD")
+    money_in_bitcoins = money_in_dollars.convert_to("Bitcoin")
+    assert_equal("0.21 Bitcoin", money_in_bitcoins.inspect)
   end
 end
